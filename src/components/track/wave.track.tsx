d7dee4a026c75b26bd33d232afe33ac5baa57de7
truong965 <22714791.truong@student.iuh.edu.vn>
2026-01-06 11:17:33 +0700

    add hard code comment

--- src/components/track/wave.track.tsx
+++ src/components/track/wave.track.tsx
@@ -5,15 +5,20 @@ import { Container } from "@mui/material";
 import { useSearchParams } from "next/navigation";
 import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import WaveSurfer, { WaveSurferOptions } from "wavesurfer.js";
+import { PauseCircleOutline, PlayArrowOutlined } from "@mui/icons-material";
+
 import './wave.scss';
+
 const WaveTrack = () => {
 
       const searchParams = useSearchParams();;
       const fileName = searchParams.get('audio');
       const containerRef = useRef<HTMLDivElement>(null);
+      const hoverRef = useRef<HTMLDivElement>(null);
+
       const [time, setTime] = useState<string>("0:00");
       const [duration, setDuration] = useState<string>("0:00");
-      const hoverRef = useRef<HTMLDivElement>(null);
+
       const optionsMemo = useMemo((): Omit<WaveSurferOptions, 'container'> => {
             let gradient, progressGradient;
             if (typeof window !== 'undefined') {
@@ -47,13 +52,8 @@ const WaveTrack = () => {
             }
       }, []);
       const wavesurfer = useWavesurfer(containerRef, optionsMemo);
-      const [isPlaying, setIsPlaying] = useState<boolean>();
-      const onPlayClick = useCallback(() => {
-            if (wavesurfer) {
-                  wavesurfer.isPlaying() ? wavesurfer.pause() : wavesurfer.play();
-                  setIsPlaying(wavesurfer.isPlaying())
-            }
-      }, [wavesurfer])
+      const [isPlaying, setIsPlaying] = useState<boolean>(false);
+
       useEffect(() => {
             if (!wavesurfer) return;
             setIsPlaying(false);
@@ -66,10 +66,16 @@ const WaveTrack = () => {
                   wavesurfer.on('play', () => setIsPlaying(true)),
                   wavesurfer.on('pause', () => setIsPlaying(false)),
                   wavesurfer.on('decode', (duration) => setDuration(formatTime(duration))),
-                  wavesurfer.on('timeupdate', (currentTime) => setTime(formatTime(currentTime)))
+                  wavesurfer.on('timeupdate', (currentTime) => setTime(formatTime(currentTime))),
+                  wavesurfer.once('interaction', () => { wavesurfer.play() })
             ]
             return () => {
                   subscriptions.forEach((unsub) => unsub());
+            }
+      }, [wavesurfer])
+      const onPlayClick = useCallback(() => {
+            if (wavesurfer) {
+                  wavesurfer.isPlaying() ? wavesurfer.pause() : wavesurfer.play();
             }
       }, [wavesurfer])
       const formatTime = (seconds: number) => {
@@ -79,15 +85,158 @@ const WaveTrack = () => {
             return `${minutes}:${paddedSeconds}`
       }
 
-      return (
-            <Container>
-                  <div ref={containerRef} className="wave-form-container">
-                        <div className="time" id="time">{time}</div>
-                        <div className="duration" id="duration">{duration}</div>
-                        <div ref={hoverRef} className="hover-wave"></div>
-                  </div>
-                  <button onClick={() => onPlayClick()}>{isPlaying === true ? "Pause" : "Play"}</button>
+      const arrComments = [
+            {
+                  id: 1,
+                  avatar: "http://localhost:8000/images/chill1.png",
+                  moment: 10,
+                  user: "username 1",
+                  content: "just a comment1"
+            },
+            {
+                  id: 2,
+                  avatar: "http://localhost:8000/images/chill1.png",
+                  moment: 30,
+                  user: "username 2",
+                  content: "just a comment3"
+            },
+            {
+                  id: 3,
+                  avatar: "http://localhost:8000/images/chill1.png",
+                  moment: 50,
+                  user: "username 3",
+                  content: "just a comment3"
+            },
+      ]
 
-            </Container>)
+      const calLeft = (moment: number) => {
+            const hardCodeDuration = 199;
+            const percent = (moment / hardCodeDuration) * 100;
+            return `${percent}%`
+      }
+      return (
+            <div style={{ marginTop: 20 }}>
+                  <div
+                        style={{
+                              display: "flex",
+                              gap: 15,
+                              padding: 20,
+                              height: 400,
+                              background: "linear-gradient(135deg, rgb(106, 112, 67) 0%, rgb(11, 15, 20) 100%)"
+                        }}
+                  >
+                        <div className="left"
+                              style={{
+                                    width: "75%",
+                                    height: "calc(100% - 10px)",
+                                    display: "flex",
+                                    flexDirection: "column",
+                                    justifyContent: "space-between"
+                              }}
+                        >
+                              <div className="info" style={{ display: "flex" }}>
+                                    <div>
+                                          <div
+                                                onClick={() => onPlayClick()}
+                                                style={{
+                                                      borderRadius: "50%",
+                                                      background: "#f50",
+                                                      height: "50px",
+                                                      width: "50px",
+                                                      display: "flex",
+                                                      alignItems: "center",
+                                                      justifyContent: "center",
+                                                      cursor: "pointer"
+                                                }}
+                                          >
+                                                {isPlaying === true ?
+                                                      <PauseCircleOutline
+                                                            sx={{ fontSize: 30, color: "white" }}
+                                                      />
+                                                      :
+                                                      <PlayArrowOutlined
+                                                            sx={{ fontSize: 30, color: "white" }}
+                                                      />
+                                                }
+                                          </div>
+                                    </div>
+                                    <div style={{ marginLeft: 20 }}>
+                                          <div style={{
+                                                padding: "0 5px",
+                                                background: "#333",
+                                                fontSize: 30,
+                                                width: "fit-content",
+                                                color: "white"
+                                          }}>
+                                                Hỏi Dân IT's song
+                                          </div>
+                                          <div style={{
+                                                padding: "0 5px",
+                                                marginTop: 10,
+                                                background: "#333",
+                                                fontSize: 20,
+                                                width: "fit-content",
+                                                color: "white"
+                                          }}
+                                          >
+                                                Eric
+                                          </div>
+                                    </div>
+                              </div>
+                              <div ref={containerRef} className="wave-form-container">
+                                    <div className="time" >{time}</div>
+                                    <div className="duration" >{duration}</div>
+                                    <div ref={hoverRef} className="hover-wave"></div>
+                                    <div className="overlay"
+                                          style={{
+                                                position: "absolute",
+                                                height: "30px",
+                                                width: "100%",
+                                                bottom: "0",
+                                                // background: "#ccc"
+                                                backdropFilter: "brightness(0.5)"
+                                          }}
+                                    ></div>
+                                    <div className="comments"
+                                          style={{ position: "relative" }}
+                                    >
+                                          {
+                                                arrComments.map(item => {
+                                                      return (
+                                                            <img
+                                                                  key={item.id}
+                                                                  style={{
+                                                                        height: 20, width: 20,
+                                                                        position: "absolute",
+                                                                        top: 71,
+                                                                        zIndex: 20,
+                                                                        left: calLeft(item.moment)
+                                                                  }}
+                                                                  src={`http://localhost:8000/images/chill1.png`}
+                                                            />
+                                                      )
+                                                })
+                                          }
+                                    </div>
+                              </div>
+                        </div>
+                        <div className="right"
+                              style={{
+                                    width: "25%",
+                                    padding: 15,
+                                    display: "flex",
+                                    alignItems: "center"
+                              }}
+                        >
+                              <div style={{
+                                    background: "#ccc",
+                                    width: 250,
+                                    height: 250
+                              }}>
+                              </div>
+                        </div>
+                  </div>
+            </div >
+      )
 }
 export default WaveTrack;
