03109d2cf54c44cf049eb7456c690880f2d9c2f5
truong965 <22714791.truong@student.iuh.edu.vn>
2026-01-07 20:17:30 +0700

    login with credentials

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,10 +3,42 @@ import GithubProvider from "next-auth/providers/github"
 import { sendRequest } from "@/utils/api"
 import { AuthOptions } from "next-auth/core/types"
 import { JWT } from "next-auth/jwt"
-
+import CredentialsProvider from "next-auth/providers/credentials";
 export const authOptions: AuthOptions = {
       secret: process.env.NO_SECRET,
       providers: [
+            CredentialsProvider({
+                  // The name to display on the sign in form (e.g. "Sign in with...")
+                  name: "Credentials",
+                  // `credentials` is used to generate a form on the sign in page.
+                  // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+                  // e.g. domain, username, password, 2FA token, etc.
+                  // You can pass any HTML attribute to the <input> tag through the object.
+                  credentials: {
+                        username: { label: "Username", type: "text" },
+                        password: { label: "Password", type: "password" }
+                  },
+                  async authorize(credentials, req) {
+                        const res = await sendRequest<IBackendRes<JWT>>({
+                              url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/auth/login`,
+                              method: "POST",
+                              body: {
+
+                                    username: credentials?.username,
+                                    password: credentials?.password,
+                              }
+                        });
+                        if (res && res.data) {
+                              // Any object returned will be saved in `user` property of the JWT
+                              return res.data as any;
+                        } else {
+                              // If you return null then an error will be displayed advising the user to check their details.
+                              return null
+
+                              // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+                        }
+                  }
+            }),
             GithubProvider({
                   clientId: process.env.GITHUB_ID!,
                   clientSecret: process.env.GITHUB_SECRET!
@@ -15,12 +47,12 @@ export const authOptions: AuthOptions = {
       callbacks: {
             jwt: async ({ token, user, account, profile, trigger }) => {
 
-                  if (trigger === "signIn" && account?.provider === "github") {
+                  if (trigger === "signIn" && account?.provider !== "credentials") {
                         const res = await sendRequest<IBackendRes<JWT>>({
                               url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/auth/social-media`,
                               method: "POST",
                               body: {
-                                    type: "CHILL",
+                                    type: account?.provider?.toLocaleUpperCase(),
                                     username: user?.email
                               }
                         });
@@ -29,6 +61,15 @@ export const authOptions: AuthOptions = {
                               token.refresh_token = res.data?.refresh_token;
                               token.user = res.data?.user;
                         }
+                  }
+                  if (trigger === "signIn" && account?.provider === "credentials") {
+                        //local data
+                        //@ts-ignore
+                        token.access_token = user.access_token;
+                        //@ts-ignore
+                        token.refresh_token = user.refresh_token;
+                        //@ts-ignore
+                        token.user = user.user;
                   }
                   return token;
             },
---
+++ src/app/api/auth/signin/page.tsx
@@ -0,0 +1,6 @@
+const SignInPage = () => {
+      return (<>
+            Sign In Page
+      </>)
+}
+export default SignInPage;
