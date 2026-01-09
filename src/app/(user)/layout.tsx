e5eb279fd394d8ae9680b1c9452a1e94c6bd1af6
truong965 <22714791.truong@student.iuh.edu.vn>
2026-01-09 12:48:42 +0700

    add ld.json

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -3,10 +3,54 @@ import AppHeader from '@/components/header/app.header';
 import AppFooter from '@/components/footer/app.footer';
 import { ReactNode } from 'react';
 import { Metadata } from 'next';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
   title: '',
   description: ''
+}
+const idJsonObject = {
+  "@context": "https://schema.org",
+  "@type": "Store",
+  "name": "Tiki",
+  "image": {
+    "@type": "ImageObject",
+    "url": "https://salt.tikicdn.com/cache/w500/ts/upload/c0/8b/46/c3f0dc850dd93bfa7af7ada0cbd75dc0.png",
+    "width": 1080,
+    "height": 1080
+  },
+  "telephone": "19006035",
+  "url": "https://tiki.vn/",
+  "address": {
+    "@type": "PostalAddress",
+    "streetAddress": "52 Ut Tich, Tan Son Nhat Ward, Ho Chi Minh City, Viet Nam",
+    "addressLocality": "Ho Chi Minh",
+    "postalCode": "700000",
+    "addressRegion": "Ho Chi Minh",
+    "addressCountry": "VN"
+  },
+  "priceRange": "1000 - 1000000000",
+  "openingHoursSpecification": [
+    {
+      "@type": "OpeningHoursSpecification",
+      "dayOfWeek": [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday"
+      ],
+      "opens": "08:00",
+      "closes": "21:00"
+    }
+  ],
+  "geo": {
+    "@type": "GeoCoordinates",
+    "latitude": "10.79664498748942",
+    "longitude": "106.65856519879867"
+  }
 }
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
@@ -15,6 +59,9 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       {children}
       <div style={{ marginBottom: "100px" }} />
       <AppFooter />
+      <Script type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(idJsonObject) }}
+        strategy='lazyOnload' />
     </>
   );
 }
